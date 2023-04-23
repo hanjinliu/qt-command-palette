5e0e41ce31e85f4506a6e2acc8998d689d316311
Hanjin Liu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2023-04-23 17:03:34 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
-from setuptools import setup
+import sys
+
+sys.stderr.write(
+    "This setup.py is not meant to be used. Please use pip to install this package."
+)
+sys.exit()
+
+from setuptools import setup  # noqa: E402
 
 setup(
     name="qt-command-palette",
